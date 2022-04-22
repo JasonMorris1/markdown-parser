6f2caea5ce51159589169efc8052ccefe6398cf3
JasonMorris1 <jasonbm123@gmail.com>
2022-04-22 16:35:56 -0700

    added junit test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,33 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void testGetLinks()
+    {
+        Path fileName = Path.of("test-file.md");
+        String content;
+        try {
+            content = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(content);
+            assertEquals("Testing for first link", links.get(0), "https://something.com");
+            assertEquals("Testing for second link", links.get(1), "some-thing.html");
+
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 }
