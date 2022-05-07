08771799ebab3ebc70c76d210f4049db77316ef9
JasonMorris1 <jasonbm123@gmail.com>
2022-05-06 17:06:02 -0700

    added make file

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,6 +8,11 @@ import java.net.URL;
 //javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
 //java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 
+
+//linux javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar  MarkdownParseTest.java
+//java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore  MarkdownParseTest
+//test
+
 import org.junit.*;
 public class MarkdownParseTest {
     @Test
